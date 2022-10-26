a1fc34eadfea8aa84d01cd6d4fc875b9ae624721
kamal0303 <kamalsharma0308@gmail.com>
2022-10-26 21:35:15 +0530

    update

---
+++ prac.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << 12.0 * 12.5;
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    x = 1111;
+    cout << x;
+
+    return 0;
+}
