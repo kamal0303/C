05fe2697d8718e638b72af9d8a95ea1aa671110e
kamal0303 <kamalsharma0308@gmail.com>
2022-10-29 22:29:36 +0530

    update

--- variables.cpp
+++ variables.cpp
@@ -7,7 +7,9 @@ int main()
     int a = 14;
     int b = 15;
     float pi = 3.14;
+    char c = 'd';
     cout << "The value of a is " << a << " \n and the value of b is " << b;
     cout << "\nThe value of pi is: " << pi;
+    cout << "\nThe value of c is: " << c;
     return 0;
 }
