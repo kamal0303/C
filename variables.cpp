c58f5dabd7724cdb8105902491603fdc4cccc914
kamal0303 <kamalsharma0308@gmail.com>
2022-10-29 22:25:03 +0530

    update

--- variables.cpp
+++ variables.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main()
 {
-    float x = 0.1;
-    x = 11.11;
-    cout << x;
-
+    int a = 14;
+    int b = 15;
+    float pi = 3.14;
+    cout << "The value of a is " << a << " \n and the value of b is " << b;
+    cout << "\nThe value of pi is: " << pi;
     return 0;
 }
